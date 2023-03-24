01f29adb0bebd98dd52cee1313ec2c9980a54c12
Kedar Navgire <kedar18.navgire@gmail.com>
2023-03-24 10:56:20 +0530

    feature: setup init.

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Kedar Navgire on 24/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
